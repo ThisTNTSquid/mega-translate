3d8c6924a3cc6e1ac43b4fab320f68d15f3efe28
ThisTNTSquid <tntsquid@corecrafted.net>
2018-02-04 03:21:40 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const translate = require("google-translate-api");
+let init = require("./input").input;
+const origin_lang=require('./input').origin_lang
+const langs = Object.keys(require("./language"));
+// const engIndex = langs.findIndex(e => e == "en");
+
+const getRand = require("./utils/random");
+
+// ------[code]-------
+
+const rounds = require("./input").rounds;
+let count = 0;
+
+function startTransalte(content, from, to, rounds) {
+  count = count + 1;
+  translate(content, { from: from, to: to }).then(res => {
+    if (count <= rounds) {
+      console.log(`${count}/${rounds} ${from} -> ${to} : ${res.text}`);
+      startTransalte(res.text, to, langs[getRand(langs.length - 1)], rounds);
+    } else {
+      translate(res.text,{from: to,to:origin_lang}).then(final=>{
+        console.log(`Completed: ${final.text}`)
+      })
+      // console.log(`Complete: ${from} -> ${to} : ${res.text}`);
+    }
+  });
+}
+console.log(`Initial: ${init}`)
+startTransalte(init, origin_lang, langs[getRand(langs.length - 1)], rounds);
